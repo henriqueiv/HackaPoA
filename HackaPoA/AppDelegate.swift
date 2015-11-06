d8be78fd2bc95b9373ebb3029d867c0bb1e345f4
Henrique Valcanaia <henrique.valcanaia@hotmail.com>
2015-11-06 04:42:53 -0200

    Commit

--- HackaPoA/AppDelegate.swift
+++ HackaPoA/AppDelegate.swift
@@ -15,8 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
+        let route = "http://Hackathon-Poa.mybluemix.net"
+        let appId = "14c1f7c0-1694-4982-b060-748ff99177ad"
+        let secret = "5c5b55785f7e46910495ec4152d3f86875e21a79"
+        IBMBluemix.initializeWithApplicationId(appId, andApplicationSecret: secret, andApplicationRoute: route)
+        
         window = UIWindow(frame: UIScreen.mainScreen().bounds)
-        let initVc = VolunteerProfileViewController()
+        let initVc = IntitutionsViewController()
         window?.rootViewController = initVc
         window?.makeKeyAndVisible()
         
